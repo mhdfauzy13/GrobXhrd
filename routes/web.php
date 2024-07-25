f4f5714d868b92c2239ba8f3f362a7b0b3bb3b11
Mhd Fauzy <moha.fauzy09@gmail.com>
2024-07-26 02:28:17 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
