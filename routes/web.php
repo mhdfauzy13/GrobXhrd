35c654e920d30ad72f09741d5728267dbbf95300
bungaputri16 <bungadevtri@gmail.com>
2024-08-14 11:47:29 +0700

    benerin route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('login');
-}
+});
+
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard.index');
