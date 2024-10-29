7a9d099050424cc7af0d02fbace4bbce9d5f8127
ananta0705 <ananta07fadhila@gmail.com>
2024-10-29 17:02:12 +0700

    perbaikan sidebar

--- resources/views/components/modal-action.blade.php
+++ resources/views/components/modal-action.blade.php
@@ -5,7 +5,7 @@
         @csrf
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Create Event</h5>
+                <h5 class="modal-title">Create Holiday</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -16,6 +16,6 @@
                 <button type="submit" class="btn btn-primary">Save changes</button>
             </div>
         </div>
-        
+
     </form>
 </div>
