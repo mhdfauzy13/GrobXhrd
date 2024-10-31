0209c1f4bff5ad5c004f26985342442af18e3f3c
ananta0705 <ananta07fadhila@gmail.com>
2024-10-31 13:27:07 +0700

    hapus duplicated data

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -12,7 +12,7 @@ class EventSeeder extends Seeder
      */
     public function run(): void
     {
-        $fake = fake('idID'); // Menggunakan Faker instance
+        Event::truncate(); // Menghapus semua data di tabel events
         $startDate = date('Y-01-01'); // Awal tahun
         $endDate = date('Y-12-31'); // Akhir tahun
 
@@ -21,33 +21,34 @@ class EventSeeder extends Seeder
         // Loop dari awal tahun ke akhir tahun
         $currentDate = $startDate;
         while ($currentDate <= $endDate) {
-            $dayOfWeek = date('w', strtotime($currentDate)); // Mengambil hari dalam format numerik (0 = Sunday, 6 = Saturday)
+            $dayOfWeek = date('w', strtotime($currentDate)); // Mengambil hari dalam format numerik (0 = Sunday)
 
             // Hanya untuk hari Minggu
             if ($dayOfWeek == 0) {
-
-                $existingEvent = array_filter($events, function ($event) use ($currentDate) {
-                    return $event['start_date'] === $currentDate;
-                });
-
-                // Jika belum ada, tambahkan event
-                if (empty($existingEvent)) {
-                    $events[] = [
-                        'title' => 'holiday', // Judul event
-                        'start_date' => $currentDate,
-                        'end_date' => $currentDate,
-                        'category' => 'danger', // Kategori untuk hari Minggu
-                        'created_at' => now(),
-                        'updated_at' => now(),
-                    ];
-                }
+                // Tambahkan event
+                $events[] = [
+                    'title' => 'holiday', // Judul event
+                    'start_date' => $currentDate,
+                    'end_date' => $currentDate,
+                    'category' => 'danger', // Kategori untuk hari Minggu
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
             }
 
             // Tambahkan satu hari
             $currentDate = date('Y-m-d', strtotime($currentDate . ' +1 day'));
         }
 
-        // Hanya simpan event yang unik
-        Event::insert($events); // Menyimpan semua event ke database
+        // Simpan atau perbarui event di database untuk menghindari duplikat
+        foreach ($events as $eventData) {
+            Event::updateOrCreate(
+                [
+                    'start_date' => $eventData['start_date'],
+                    'title' => $eventData['title'],
+                ],
+                $eventData
+            );
+        }
     }
 }
